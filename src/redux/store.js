59972b6bba49fe0a314d327d960fbfe45981ee87
Poovendharan Selvaraj <97455003+PoovendharanSelvaraj@users.noreply.github.com>
2022-06-29 21:07:06 +0530

    reduxThunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
-import {legacy_createStore} from "redux";
+import {applyMiddleware, legacy_createStore} from "redux";
 import { reducer } from "./reducers";
+import thunk from "redux-thunk";
 
 
-const store = legacy_createStore(reducer);
+const store = legacy_createStore(reducer,applyMiddleware(thunk));
 
 export {store};
